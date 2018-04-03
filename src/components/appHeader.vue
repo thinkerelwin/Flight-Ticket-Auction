b24427fcbc1e334252c2f9ed9ad9a78f96125f5e
elwin <elwin@farener.com>
2018-04-03 20:19:51 +0800

    change farener-log.png to Farener-log.png

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <router-link class="navbar-brand" to="/">
-        <img src="../assets/farener-logo.png" alt="farener logo">
+        <img src="../assets/Farener-logo.png" alt="farener logo">
       </router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
